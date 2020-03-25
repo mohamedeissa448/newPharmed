2bb58f668b7df1e8e0de470e927be11c098767bc
mohamedeissa448 <mohamedeissa448@gmail.com>
2020-03-25 16:22:26 +0200

    add authentication module

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,10 +1,31 @@
-﻿import { Routes, RouterModule } from '@angular/router';
-import { ModuleWithProviders } from '@angular/core';
+﻿import { Routes, RouterModule } from "@angular/router";
+import { ModuleWithProviders } from "@angular/core";
 
 export const routes: Routes = [
-    { path: '', redirectTo:'dashboard', pathMatch: 'full'},
-    { path: 'sys-setup', loadChildren: () => import('../app/components/sys-setup/sys-setup.module').then(m => m.SysSetupModule) },
-    { path: 'dashboard', loadChildren: () => import('../app/components/dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  {
+    path: "sys-setup",
+    loadChildren: () =>
+      import("../app/components/sys-setup/sys-setup.module").then(
+        m => m.SysSetupModule
+      )
+  },
+  {
+    path: "dashboard",
+    loadChildren: () =>
+      import("../app/components/dashboard/dashboard.module").then(
+        m => m.DashboardModule
+      )
+  },
+  {
+    path: "authentication",
+    loadChildren: () =>
+      import("../app/authentication/authentication.module").then(
+        m => m.AuthenticationModule
+      )
+  }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: false });
+export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {
+  useHash: false
+});
